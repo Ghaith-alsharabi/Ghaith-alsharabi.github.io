cad3264201f70e8cc20db5d3c91ac3329f92ce13
Ghaith <ghais_93@hotmail.com>
2021-03-12 08:14:03 +0100

    Create root_page_JS.js

---
+++ root_page_JS.js
@@ -0,0 +1,51 @@
+function myFunction() {
+myFunction("http://192.168.1.107/");
+delay(2);
+myFunction("http://192.168.1.107/");
+delay(2);
+myFunction("http://192.168.1.107/");
+}
+
+function myFunction(ip) {
+    var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", ip, true);
+  xhttp.send();
+}
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperature").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature", true);
+  xhttp.send();
+}, 10000 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 10000 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("motion").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/motion", true);
+  xhttp.send();
+}, 10000 ) ;
