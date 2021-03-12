1261f6cc44a4cc551a6e78c9502e1d3f74ad76af
Ghaith <ghais_93@hotmail.com>
2021-03-12 09:41:37 +0100

    Update root_page_JS.js

--- root_page_JS.js
+++ root_page_JS.js
@@ -29,14 +29,11 @@ setInterval(function ( ) {
   xhttp.send();
 }, 10000 ) ;
 
-
-
 setInterval(function ( ) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("gas").innerHTML = this.responseText;
-        console_log(this.responseText);
     }
   };
   xhttp.open("GET", "/gas", true);
